ce8f66d3f87734ce69d8d4044570ed7494d868c9
guohuliu <1774687347@qq.com>
2020-09-19 18:19:00 +0800

    feature: 登陆退出功能实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-      Home View
-    </div>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
   export default {
+    data () {
+      return {}
+    },
+    methods: {
+      logout () {
+        window.sessionStorage.clear();
+        this.$router.push("/login");
+      }
+    }
   }
 </script>
 
