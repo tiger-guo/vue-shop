28ecb47985635012dfd0e62e37c8ba41a508b8d4
guohuliu <1774687347@qq.com>
2020-09-19 18:14:29 +0800

    feature: 登陆框 挂载路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -23,4 +23,20 @@ const router = new VueRouter({
   mode: 'history'
 })
 
+// 挂载路由导航守卫
+// params:
+//    to 将要访问的路径
+//    form 从哪个路径跳转而来
+//    next 是一个函数，表示放行 [ next('/login') 表示强制跳转 ]
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    return next();
+  }
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) {
+    return next('/login');
+  }
+  next();
+})
+
 export default router
